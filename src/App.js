83c6060fc7fbfa367955e5eab80ea6692dea584a
SETU FULAWADE <setufulawade12@gmail.com>
2024-04-26 18:45:04 +0530

    test_2

--- src/App.js
+++ src/App.js
@@ -1,12 +1,18 @@
 import './App.css';
 import Navbar from './components/Navbar';
+import Textform1 from './components/Textform1';
 
 function App() {
   return (
     <>
       <Navbar title='TextUtils' aboutText='About TextUtils' />
+
+      <div className="container my-4">
+        <Textform1 heading="Enter the text to analyze" />
+      </div>
     </>
   );
 }
 
 export default App;
+
---
+++ src/components/Textform1.js
@@ -0,0 +1,26 @@
+import React,{ useState } from 'react'
+
+export default function Textform1(props) {
+  const [text, setText] = useState('Enter text here');
+
+  const handleUpclick = ()=> {
+    console.log('Uppercase was clicked' +text)
+    let newText = text.toUpperCase();
+    setText(newText);  
+  }
+
+  const hanndleonchange = (event) => {
+    console.log('onchange')
+    setText(event.target.value)
+  }
+
+  return (
+    <div>
+      <h1>{props.heading} </h1>
+      <div className="mb-3">
+        <textarea className="form-control" value={text} onChange={hanndleonchange} id="myBox" rows="8"></textarea>
+      </div>
+      <button className="btn btn-primary" onClick={handleUpclick} >Convert to UpperCase</button>
+    </div>
+  )
+}
